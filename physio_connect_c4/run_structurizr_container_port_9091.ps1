7d516b22ad4b12282d2b69ee68b3acc3d397dae0
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2022-12-25 16:21:40 +0100

    Docker-compose and ps1 script now always pull images to ensure everyone uses the same images

--- physio_connect_c4/run_structurizr_container_port_9091.ps1
+++ physio_connect_c4/run_structurizr_container_port_9091.ps1
@@ -1,1 +1,2 @@
-docker run -it --rm -p 9091:8080 -v ${PWD}:/usr/local/structurizr structurizr/lite
+docker pull structurizr/lite:latest
+docker run -it --rm -p 9091:8080 -v ${PWD}:/usr/local/structurizr structurizr/lite:latest
